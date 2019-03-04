066f3056a86c46e381d812448909269b3451e19f
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2019-03-04 11:26:40 +0100

    Add first integration test

--- tests/run.sh
+++ tests/run.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+readonly BIN=$1
+
 function main
 {
+  $BIN/evanescent.exe &
+  local readonly pid=$!
+
+  sleep 0.01
+  curl \
+    -H "User-Agent: Robocop" \
+    -X PUT -d "Murphy" \
+    http://127.0.0.1:9000/OmniConsumerProducts
+
+  kill $pid
+
+  wait
   echo OK
 }
 
